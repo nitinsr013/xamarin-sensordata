694d6431814af8fff6546e69d1c38468afe2392f
automation <nitin.srivastav440@gmail.com>
2017-07-25 15:25:40 +0530

    Read data from mobile sensor

---
+++ MotionDetector/MainActivity.cs
@@ -0,0 +1,65 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Hardware;
+using Android.Content;
+
+namespace MotionDetector
+{
+    [Activity(Label = "MotionDetector", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity, ISensorEventListener
+    {
+        static readonly object _syncLock = new object();
+        SensorManager _sensorManager;
+        TextView _sensorTextView;
+        TextView _sensorGyroscopeTextView;        
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            _sensorManager = (SensorManager)GetSystemService(Context.SensorService);
+            _sensorTextView = FindViewById<TextView>(Resource.Id.accelerometer_text);
+            _sensorGyroscopeTextView = FindViewById<TextView>(Resource.Id.gyroscope_text);
+        }
+        public void OnAccuracyChanged(Sensor sensor, SensorStatus accuracy)
+        {
+            // We don't want to do anything here.
+        }
+
+        public void OnSensorChanged(SensorEvent e)
+        {
+
+            lock (_syncLock)
+            {
+                if (e.Sensor.Type == SensorType.Accelerometer)
+                {
+                    _sensorTextView.Text = string.Format("x={0:f}, y={1:f}, z={2:f}", e.Values[0], e.Values[1], e.Values[2]);
+                }
+                if (e.Sensor.Type == SensorType.Gyroscope)
+                {
+                    _sensorGyroscopeTextView.Text = string.Format("xg={0:f}, yg={1:f}, zg={2:f}", e.Values[0], e.Values[1], e.Values[2]);
+                }
+            }
+
+
+        }
+        protected override void OnResume()
+        {
+            base.OnResume();
+            _sensorManager.RegisterListener(this,
+                                            _sensorManager.GetDefaultSensor(SensorType.Accelerometer),
+                                            SensorDelay.Ui);
+            _sensorManager.RegisterListener(this,
+                                            _sensorManager.GetDefaultSensor(SensorType.Gyroscope),
+                                            SensorDelay.Ui);
+        }
+        protected override void OnPause()
+        {
+            base.OnPause();
+            _sensorManager.UnregisterListener(this);
+        }
+
+
+    }
+}
+
---
+++ MotionDetector/obj/Debug/android/src/md5a150bff9132ef2cb3989a995a27742b6/MainActivity.java
@@ -0,0 +1,84 @@
+package md5a150bff9132ef2cb3989a995a27742b6;
+
+
+public class MainActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer,
+		android.hardware.SensorEventListener
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onResume:()V:GetOnResumeHandler\n" +
+			"n_onPause:()V:GetOnPauseHandler\n" +
+			"n_onAccuracyChanged:(Landroid/hardware/Sensor;I)V:GetOnAccuracyChanged_Landroid_hardware_Sensor_IHandler:Android.Hardware.ISensorEventListenerInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"n_onSensorChanged:(Landroid/hardware/SensorEvent;)V:GetOnSensorChanged_Landroid_hardware_SensorEvent_Handler:Android.Hardware.ISensorEventListenerInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"";
+		mono.android.Runtime.register ("MotionDetector.MainActivity, MotionDetector, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("MotionDetector.MainActivity, MotionDetector, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onResume ()
+	{
+		n_onResume ();
+	}
+
+	private native void n_onResume ();
+
+
+	public void onPause ()
+	{
+		n_onPause ();
+	}
+
+	private native void n_onPause ();
+
+
+	public void onAccuracyChanged (android.hardware.Sensor p0, int p1)
+	{
+		n_onAccuracyChanged (p0, p1);
+	}
+
+	private native void n_onAccuracyChanged (android.hardware.Sensor p0, int p1);
+
+
+	public void onSensorChanged (android.hardware.SensorEvent p0)
+	{
+		n_onSensorChanged (p0);
+	}
+
+	private native void n_onSensorChanged (android.hardware.SensorEvent p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
